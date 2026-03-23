33c04a3a1d459bd9cbcde573a8d28c50345aa305
tusharkr02p <tushar033531@gmail.com>
2026-03-23 20:35:40 +0530

    last done

--- fullstack-chat-app/backend/src/index.js
+++ fullstack-chat-app/backend/src/index.js
@@ -31,13 +31,12 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
 
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
-
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
