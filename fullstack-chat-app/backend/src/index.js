f26840d196bd1a0ab250edb83ae778e127631af5
tusharkr02p <tushar033531@gmail.com>
2026-03-23 21:08:00 +0530

    done

--- fullstack-chat-app/backend/src/index.js
+++ fullstack-chat-app/backend/src/index.js
@@ -14,7 +14,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
